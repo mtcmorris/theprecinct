2f5383f16d98a706d99ee556c96b2cb3452f6694
Michael Morris <mtcmorris@gmail.com>
2012-03-30 18:16:19 +1100

    Chimney is gone :O

--- coffee/app.coffee
+++ coffee/app.coffee
@@ -1,33 +1,3 @@
-$(document).ready ->
-  chimney = $("div.chimney-wrap")
-  puff = ->
-
-    paths = ["path-one", "path-two"]
-    colors = ["#ddd", "#999", "#ccc", "#888", "#aaa"]
-    size = Math.floor( Math.random()* 8 ) + 10
-
-    path  = pickone paths
-    color = pickone colors
-
-
-    smoke = $("""
-      <span class='puff #{path}'
-        style='width:#{size}px;
-               height:#{size}px;
-               background-color: #{color};'>
-    """).appendTo chimney
-
-    setTimeout puff, 200
-    setTimeout ->
-      smoke.remove()
-    , 4000
-
-  setTimeout puff, 500
-
-window.pickone = (array) ->
-  array[Math.floor( Math.random()*array.length )]
-
-
 $("nav a").click ->
   dir = $($(this).attr("href")).attr("class")
   $("#box").removeClass().addClass("show-#{dir}")
--- js/app.js
+++ js/app.js
@@ -1,28 +1,5 @@
 (function() {
 
-  $(document).ready(function() {
-    var chimney, puff;
-    chimney = $("div.chimney-wrap");
-    puff = function() {
-      var color, colors, path, paths, size, smoke;
-      paths = ["path-one", "path-two"];
-      colors = ["#ddd", "#999", "#ccc", "#888", "#aaa"];
-      size = Math.floor(Math.random() * 8) + 10;
-      path = pickone(paths);
-      color = pickone(colors);
-      smoke = $("<span class='puff " + path + "'\n  style='width:" + size + "px;\n         height:" + size + "px;\n         background-color: " + color + ";'>").appendTo(chimney);
-      setTimeout(puff, 200);
-      return setTimeout(function() {
-        return smoke.remove();
-      }, 4000);
-    };
-    return setTimeout(puff, 500);
-  });
-
-  window.pickone = function(array) {
-    return array[Math.floor(Math.random() * array.length)];
-  };
-
   $("nav a").click(function() {
     var dir;
     dir = $($(this).attr("href")).attr("class");
